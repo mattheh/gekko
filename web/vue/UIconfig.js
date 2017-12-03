6e3a40d4a62bc3b96b504295e946f452cec272f5
Matthew Hill <mhill@redhat.com>
2017-12-03 16:14:12 -0500

    Update Config

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,17 +4,17 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
     host: '127.0.0.1',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
-    ssl: false,
-    host: 'localhost',
+    ssl: true,
+    host: 'gekko-mh.herokuapp.com',
     port: 3000,
-    path: '/'
+    path: '/home'
   },
   adapter: 'sqlite'
 }
